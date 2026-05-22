e01aacf31ec4915627db4f7f450cbc1e11d13440
Pavel Dubrova <pashadubrova@gmail.com>
2026-05-22 13:52:22 +0300

    ASoC: kalama: Fix formatting issues
    Various changes to fix format specifiers in Kalama machine driver.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- asoc/kalama.c
+++ asoc/kalama.c
@@ -1350,7 +1350,7 @@ static int msm_snd_card_late_probe(struct snd_soc_card *card)
 	if (!rtd) {
 		dev_err(card->dev,
 			"%s: snd_soc_get_pcm_runtime for %s failed!\n",
-			__func__, card->dai_link[0]);
+			__func__, card->dai_link[0].name);
 		return -EINVAL;
 	}
 
@@ -1822,7 +1822,7 @@ static int kalama_ssr_enable(struct device *dev, void *data)
 	if (!rtd_wcd) {
 		dev_dbg(dev,
 			"%s: snd_soc_get_pcm_runtime for %s failed!\n",
-			__func__, card->dai_link[0]);
+			__func__, card->dai_link[0].name);
 	}
 
 	if (pdata->wsa_max_devs > 0) {
@@ -1831,7 +1831,7 @@ static int kalama_ssr_enable(struct device *dev, void *data)
 		if (!rtd_wsa) {
 			dev_dbg(dev,
 			"%s: snd_soc_get_pcm_runtime for %s failed!\n",
-			__func__, card->dai_link[ARRAY_SIZE(msm_rx_tx_cdc_dma_be_dai_links)]);
+			__func__, card->dai_link[ARRAY_SIZE(msm_rx_tx_cdc_dma_be_dai_links)].name);
 		}
 	}
 	/* set UPD configuration */
